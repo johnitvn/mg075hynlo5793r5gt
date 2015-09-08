c38aa4be1e0326c96b3cde8484c11558d213758c
John Martin <john.itvn@gmail.com>
2015-09-08 23:02:27 +0700

    Add feature create employee console command

--- source/backend/models/Employee.php
+++ source/backend/models/Employee.php
@@ -2,6 +2,7 @@
 
 namespace backend\models;
 
+use Yii;
 use \backend\models\base\Employee as BaseEmployee;
 use yii\web\IdentityInterface;
 
@@ -52,6 +53,34 @@ class Employee extends BaseEmployee implements IdentityInterface {
      */
     public static function findIdentityByAccessToken($token, $type = null) {
         throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return boolean if password provided is valid for current user
+     */
+    public function validatePassword($password) {
+        return Yii::$app->security->validatePassword($password, $this->password_hash);
+    }
+
+    /**
+     * Generates password hash from password and sets it to the model
+     *
+     * @param string $password
+     */
+    public function setPassword($password) {
+        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+    }
+
+    public function beforeSave($insert) {
+        if (!parent::beforeSave($insert)) {
+            return false;
+        } else if ($this->password !== null) {
+            $this->setPassword($this->password);
+        }
+        return true;
     }
 
 }
---
+++ source/console/controllers/EmployeeController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace console\controllers;
+
+use Yii;
+use yii\console\Controller;
+use backend\models\Employee;
+
+/**
+ * Manager Employee
+ *
+ * @author john
+ */
+class EmployeeController extends Controller {
+
+    const EMAIL_REGEX = '/^[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/';
+
+    /**
+     * dd/mm/YY
+     */
+    const DATE_REGEX = '/(0[1-9]|[12][0-9]|3[01])[- \/.](0[1-9]|1[012])[- \/.](19|20)\d\d/';
+
+    public function actionTest() {
+        $model = Employee::findIdentity(2);
+        var_dump($model->birthday);
+    }
+
+    /**
+     * Create employee
+     */
+    public function actionCreate() {
+        $model = new Employee([
+            'scenario' => 'create'
+        ]);
+
+        $this->stdout("\nPlease type all infomation of employee\n");
+        $this->stdout("Field marked with (*) is required\n");
+        $this->stdout("All field left you can live empty for default\n");
+        $info = $this->promptEmployeeinfo($model);
+        Yii::configure($model, $info);
+
+        if (!$model->validate()) {
+            echo "\nHave something wrong. Please fix all folowing bellow:\n";
+            $this->printErrors($model);
+            echo "\n";
+            exit();
+        } else if (!$model->save()) {
+            echo "\nHave something wrong. Please fix all folowing bellow:\n";
+            exit();
+        } else {
+            echo "\nEmployee have been create success!!!\n";
+        }
+    }
+
+    private function promptEmployeeinfo($model) {
+        $info = [];
+        $info['fullname'] = $this->prompt($model->getAttributeLabel('fullname') . " (*) :", [ 'required' => true]);
+        $info['username'] = $this->prompt($model->getAttributeLabel('username') . " (*) :", [ 'required' => true, 'pattern' => \backend\models\base\Employee::USERNAME_REGEXP]);
+        $info['email'] = $this->prompt($model->getAttributeLabel('email') . " (*) :", [ 'required' => true, 'pattern' => self::EMAIL_REGEX]);
+        $info['password'] = $this->prompt($model->getAttributeLabel('password') . "(*):", [ 'required' => true]);
+        $info['confirm_password'] = $this->prompt($model->getAttributeLabel('password') . "(*):", [ 'required' => true]);
+        return $info;
+    }
+
+    private function printErrors($model) {
+        foreach ($model->getErrors() as $field => $errors) {
+            $this->stdout(" - " . $model->getAttributeLabel($field) . "\n");
+            foreach ($errors as $msg) {
+                $this->stdout("     - $msg\n");
+            }
+        }
+    }
+
+}
