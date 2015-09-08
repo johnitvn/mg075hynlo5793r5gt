861c7b50638df58432a2362b38e339687929d3bf
John Martin <john.itvn@gmail.com>
2015-09-08 12:57:17 +0700

    Add base classes for unit test

---
+++ source/tests/codeception/console/unit/DbTestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace tests\codeception\console\unit;
+
+class DbTestCase extends \yii\codeception\DbTestCase {
+
+    public $appConfig = '@tests/codeception/config/console/unit.php';
+
+}
---
+++ source/tests/codeception/console/unit/TestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace tests\codeception\console\unit;
+
+class TestCase extends \yii\codeception\TestCase {
+
+    public $appConfig = '@tests/codeception/config/console/unit.php';
+
+}
