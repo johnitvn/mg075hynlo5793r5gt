3c13e00791f49498ec38d2e4c5b3d57983098b11
John Martin <john.itvn@gmail.com>
2015-09-08 13:44:26 +0700

    Add source code passed test

--- source/console/components/BaseMigration.php
+++ source/console/components/BaseMigration.php
@@ -3,6 +3,7 @@
 namespace console\components;
 
 use yii\db\Migration;
+use yii\base\InvalidConfigException;
 
 /**
  * Base class for all application migration
@@ -10,5 +11,16 @@ use yii\db\Migration;
  * @author john
  */
 class BaseMigration extends Migration {
-    
+
+    protected $tableOptions;
+
+    public function init() {
+        parent::init();
+        if ($this->db->driverName === 'mysql') {
+            $this->tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        } else {
+            throw new InvalidConfigException($this->db->driverName . " is not support");
+        }
+    }
+
 }
