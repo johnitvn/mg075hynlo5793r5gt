05ccd383d21d1978f8072db689e5b87624f0490d
John Martin <john.itvn@gmail.com>
2015-09-09 19:06:47 +0700

    Add behaviors and add process code for action in

--- source/backend/controllers/SignController.php
+++ source/backend/controllers/SignController.php
@@ -2,15 +2,38 @@
 
 namespace backend\controllers;
 
+use Yii;
 use yii\web\Controller;
+use yii\filters\VerbFilter;
 
 /**
  * Sign controller
  */
 class SignController extends Controller {
 
+    /**
+     * @inheritdoc
+     */
+    public function behaviors() {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [ 'actions' => ['in'], 'allow' => true,],
+                    [ 'actions' => ['out'], 'allow' => true, 'roles' => ['@']],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [ 'logout' => ['post']],
+            ],
+        ];
+    }
+
     public function actionIn() {
-        
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
     }
 
     public function actionOut() {
