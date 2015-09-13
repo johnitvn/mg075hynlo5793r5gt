c795015abca26a5a919a8ee585913790d15a2ea7
John Martin <john.itvn@gmail.com>
2015-09-13 21:05:59 +0700

    Add feature film actor manager

---
+++ source/backend/models/FilmActor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use \backend\models\base\FilmActor as BaseFilmActor;
+
+/**
+ * This is the model class for table "film_actor".
+ */
+class FilmActor extends BaseFilmActor
+{
+}
